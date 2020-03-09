78f80823f97d695d2094cd6b19aacdb7748c6898
Molly Novash <novash@pdx.edu>
2020-03-09 00:00:25 -0700

    working on watchlist

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,9 +3,11 @@
 
 import React from "react";
 import "../index.css";
-import { Media, Container, Row, Button} from 'reactstrap';
+import { Media, Container, Row, Button } from "reactstrap";
 import { NavLink } from "react-router-dom";
 import { TiPlusOutline, TiMinusOutline } from "react-icons/ti";
+import { auth, db } from "./Firebase";
+import firebase from "firebase";
 
 class Movie extends React.Component {
   constructor(props) {
@@ -15,81 +17,194 @@ class Movie extends React.Component {
       movies: {},
       loading: true
     };
+
+    this.addToWatchlist = this.addToWatchlist.bind(this);
+    this.removeFromWatchlist = this.removeFromWatchlist.bind(this);
   }
 
-  componentDidMount(){
-    fetch("https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"+this.props.location.id, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-                "x-rapidapi-key": "c3bfcbae69msh2fefac5f7da67adp1d2687jsnade70055f49b"
-            }
-    })
-    .then(response => {
-      response.json().then (data =>{
+  addToWatchlist() {
+    var watchlistRef = db.collection("user").doc(auth.currentUser.email);
+    watchlistRef.update({
+      watchlist: firebase.firestore.FieldValue.arrayUnion(this.state.movie.id)
+    });
+    alert("adding to watchlist");
+  }
+
+  removeFromWatchlist() {
+    var watchlistRef = db.collection("user").doc(auth.currentUser.email);
+    watchlistRef.update({
+      watchlist: firebase.firestore.FieldValue.arrayRemove(this.state.movie.id)
+    });
+    alert("removing from watchlist");
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" +
+        this.props.location.id,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host":
+            "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+          "x-rapidapi-key": "c3bfcbae69msh2fefac5f7da67adp1d2687jsnade70055f49b"
+        }
+      }
+    )
+      .then(response => {
+        response.json().then(data => {
           this.setState({
             movie: data
-          }) 
-          console.log(this.state.movie)
+          });
+          console.log(this.state.movie);
+        });
       })
-    })
-    .catch(err => {
+      .catch(err => {
         console.log(err);
-    }); 
+      });
 
-    fetch('https://www.omdbapi.com/?apikey=1e54e73e&i='+this.props.location.id)
-    .then(response => {
-      response.json().then (data =>{
+    fetch(
+      "https://www.omdbapi.com/?apikey=1e54e73e&i=" + this.props.location.id
+    )
+      .then(response => {
+        response.json().then(data => {
           this.setState({
             movies: data
-          })
-          console.log(this.state.movies)
+          });
+          console.log(this.state.movies);
+        });
       })
-    })
-    .catch(err => {
+      .catch(err => {
         console.log(err);
-    }); 
+      });
   }
-  
+
   // below I added a link back to the landing page, just to make our lives easier while testing
   // we'll remove it when we actually build the events page, since users won't need to go back to
   // the landing page
   render() {
     return (
       <Container>
-        {this.state.movie? 
-        <div>
-          <Row>
-            <a title="if u stan imdb..." className="movie-title" rel="noopener noreferrer" target="_blank" href={"https://www.imdb.com/title/"+this.state.movie.id}>{this.state.movie.title} ({this.state.movie.year})</a>
-          </Row>
-          <Row>
-            <div className="movie-poster">
-              <a title="trailer is here!" className="movie-title" rel="noopener noreferrer" target="_blank" href={this.state.movie.trailer?this.state.movie.trailer.link:"/"}>
-                <img className="movie-actual-poster" alt={this.state.movie.title} src={this.state.movie.poster}/>
+        {this.state.movie ? (
+          <div>
+            <Row>
+              <a
+                title="if u stan imdb..."
+                className="movie-title"
+                rel="noopener noreferrer"
+                target="_blank"
+                href={"https://www.imdb.com/title/" + this.state.movie.id}
+              >
+                {this.state.movie.title} ({this.state.movie.year})
               </a>
-            </div>
-          </Row>
-          <Row><div className="movie-director">directed by: {this.state.movies.Director}</div></Row>
-          <Row><div  className="movie-info">{this.state.movies.Rated} | {this.state.movies.Runtime} | {this.state.movies.Released}</div></Row>
-          <Row><div  className="movie-info">{this.state.movies.Country} | {this.state.movies.Language}</div></Row>
-          <Row><div  className="movie-info">IMDb rating: {this.state.movies.imdbRating} | Metascore: {this.state.movies.Metascore}</div></Row>
-          <Row><div  className="movie-director">{this.state.movies.Production?this.state.movies.Production:""}</div></Row>
-          <Row><div  className="movie-info">AWARDS: {this.state.movies.Awards}</div></Row>
-          <Row><div  className="movie-info">PLOT: {this.state.movies.Plot}</div></Row>
-          {this.state.movie.cast?
-          this.state.movie.cast.map(item=>{
-            return(
-              <Row key=""><div  className="movie-actor">
-                <a className="movie-actor-link" rel="noopener noreferrer" target="_blank" href={'https://www.imdb.com/name/'+item.actor_id}>{item.actor}</a> as {item.character}
-              </div></Row>
-            )
-          })
-          :""}
-          
-          <Row><div className="movie-btn"> <Button className="movie-rl-btn"><TiPlusOutline /> watchlist</Button></div></Row>
-          <Row><div className="movie-btn"> <Button className="movie-rl-btn"><TiMinusOutline /> watchlist</Button></div></Row>
-        </div>
-        : <Media>wait a sec bru</Media>}
+            </Row>
+            <Row>
+              <div className="movie-poster">
+                <a
+                  title="trailer is here!"
+                  className="movie-title"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  href={
+                    this.state.movie.trailer
+                      ? this.state.movie.trailer.link
+                      : "/"
+                  }
+                >
+                  <img
+                    className="movie-actual-poster"
+                    alt={this.state.movie.title}
+                    src={this.state.movie.poster}
+                  />
+                </a>
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-director">
+                directed by: {this.state.movies.Director}
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-info">
+                {this.state.movies.Rated} | {this.state.movies.Runtime} |{" "}
+                {this.state.movies.Released}
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-info">
+                {this.state.movies.Country} | {this.state.movies.Language}
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-info">
+                IMDb rating: {this.state.movies.imdbRating} | Metascore:{" "}
+                {this.state.movies.Metascore}
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-director">
+                {this.state.movies.Production
+                  ? this.state.movies.Production
+                  : ""}
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-info">
+                AWARDS: {this.state.movies.Awards}
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-info">PLOT: {this.state.movies.Plot}</div>
+            </Row>
+            {this.state.movie.cast
+              ? this.state.movie.cast.map(item => {
+                  return (
+                    <Row key="">
+                      <div className="movie-actor">
+                        <a
+                          className="movie-actor-link"
+                          rel="noopener noreferrer"
+                          target="_blank"
+                          href={"https://www.imdb.com/name/" + item.actor_id}
+                        >
+                          {item.actor}
+                        </a>{" "}
+                        as {item.character}
+                      </div>
+                    </Row>
+                  );
+                })
+              : ""}
+
+            <Row>
+              <div className="movie-btn">
+                <Button
+                  className="movie-rl-btn"
+                  onClick={() => {
+                    this.addToWatchlist();
+                  }}
+                >
+                  <TiPlusOutline /> watchlist
+                </Button>
+              </div>
+            </Row>
+            <Row>
+              <div className="movie-btn">
+                {" "}
+                <Button
+                  className="movie-rl-btn"
+                  onClick={() => {
+                    this.removeFromWatchlist();
+                  }}
+                >
+                  <TiMinusOutline /> watchlist
+                </Button>
+              </div>
+            </Row>
+          </div>
+        ) : (
+          <Media>wait a sec bru</Media>
+        )}
       </Container>
     );
   }
--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -3,11 +3,79 @@
 import React from "react";
 import "../index.css";
 import { NavLink } from "react-router-dom";
+import { auth, db } from "./Firebase";
+import firebase, { DocumentReference, DocumentSnapshot } from "firebase";
+
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { FaImdb } from "react-icons/fa";
+import { TiMediaFastForward } from "react-icons/ti";
+import Icon from "react-fa";
+import rp from "request-promise";
+import cheerio from "cheerio";
+import Carousel from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import ComingSoon from "./comingSoon";
 
 class Watchlist extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value: 0,
+      apis: []
+    };
+  }
+
+  async componentDidMount() {
+    if (auth.currentUser != null) {
+      var docRef = db.collection("user").doc(auth.currentUser.email);
+
+      docRef
+        .get()
+        .then(function(doc) {
+          if (doc.exists) {
+            console.log("watchlist:", doc.data().watchlist);
+
+            console.log("length of watchlist:", doc.data().watchlist.length);
+            let apis = [];
+            for (let i = 0; i < doc.data().watchlist.length; i++) {
+              let name = doc.data().watchlist[i];
+              fetch(
+                "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/title/" +
+                  name,
+                {
+                  method: "GET",
+                  mode: "no-cors",
+                  headers: {
+                    "x-rapidapi-host":
+                      "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+                    "x-rapidapi-key":
+                      "c3bfcbae69msh2fefac5f7da67adp1d2687jsnade70055f49b"
+                  }
+                }
+              )
+                .then(response => {
+                  response.json().then(data => {
+                    apis.push(data);
+                  });
+                })
+                .catch(err => {
+                  console.log(err);
+                });
+            }
+            this.setState({
+              apis: apis
+            });
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        })
+        .catch(function(error) {
+          console.log("Error getting document:", error);
+        });
+    } else {
+      console.log("currentUser is null");
+    }
   }
 
   // below I added a link back to the landing page, just to make our lives easier while testing
@@ -15,21 +83,66 @@ class Watchlist extends React.Component {
   // the landing page
   render() {
     return (
-      <div>
-        <div>
-          <p>Watchlist page component</p>
-          <p>Can be found in src/components/Watchlist.js</p>
-        </div>
-        <div>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-          </ul>
-        </div>
+      <div className="carousel-inners">
+        <h2 className="section-title">Watchlist</h2>
+
+        <Carousel
+          value={this.state.value}
+          onChange={this.onChange}
+          arrowLeft={<Icon name="angle-double-left" />}
+          arrowLeftDisabled={<Icon name="angle-left" />}
+          arrowRight={<Icon name="angle-double-right" />}
+          arrowRightDisabled={<Icon name="angle-right" />}
+          addArrowClickHandler
+          clickToChange
+          autoPlay={4000}
+          stopAutoPlayOnHover
+          slidesPerPage={3}
+          infinite
+          className="carousel-item"
+        >
+          {this.state.apis.length === 0 ? (
+            <div>i&apos;m gettin there...</div>
+          ) : (
+            this.state.apis.map(item => {
+              return (
+                <Card key={item.title} className="card">
+                  <a
+                    title="traaaailer"
+                    href={item.trailer.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <img alt={item.title} src={item.poster} />
+                  </a>
+                  <CardBody>
+                    <CardTitle>
+                      <NavLink
+                        to={{
+                          pathname: "./Movie",
+                          id: item.id
+                        }}
+                        className="card-title"
+                      >
+                        {item.title}
+                      </NavLink>
+                    </CardTitle>
+                    <CardSubtitle>
+                      <FaImdb />
+                      {item.rating}
+                    </CardSubtitle>
+                    <CardSubtitle>
+                      <TiMediaFastForward />
+                      {item.length}
+                    </CardSubtitle>
+                    {/*<CardSubtitle>Director: </CardSubtitle>*/}
+                    <CardText>Plot: {item.plot}</CardText>
+                  </CardBody>
+                </Card>
+              );
+            })
+          )}
+        </Carousel>
       </div>
     );
-  }
-}
-
-export default Watchlist;
+ 
